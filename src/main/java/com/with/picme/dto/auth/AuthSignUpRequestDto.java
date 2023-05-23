d318c44917e41d4912bba311e72c9d799f6bae25
yangjiyoung <0112jyoung@naver.com>
2023-05-23 20:47:17 +0900

    feat: dto 함수 추가

--- src/main/java/com/with/picme/dto/auth/AuthSignUpRequestDto.java
+++ src/main/java/com/with/picme/dto/auth/AuthSignUpRequestDto.java
@@ -1,8 +1,18 @@
 package com.with.picme.dto.auth;
 
+import com.with.picme.entity.User;
+
 public record AuthSignUpRequestDto(
         String email,
         String password,
         String username
 ) {
+    public User toEntity(){
+        return User
+                .builder()
+                .email(this.email)
+                .name(this.username)
+                .password(this.password)
+                .build();
+    }
 }
--- src/main/java/com/with/picme/dto/auth/AuthSignUpResponseDto.java
+++ src/main/java/com/with/picme/dto/auth/AuthSignUpResponseDto.java
@@ -1,8 +1,20 @@
 package com.with.picme.dto.auth;
 
+import com.with.picme.entity.User;
+import lombok.Builder;
+
+@Builder
 public record AuthSignUpResponseDto(
         Long id,
         String userName,
         String accessToken
 ) {
+    public static AuthSignUpResponseDto from(User user, String accessToken){
+        return AuthSignUpResponseDto
+                .builder()
+                .id(user.getId())
+                .accessToken(accessToken)
+                .userName(user.getName())
+                .build();
+    }
 }
