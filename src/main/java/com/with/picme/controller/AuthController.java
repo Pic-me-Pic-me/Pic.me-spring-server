4dcd8450ec689b9f6da2905183e3569040611d5e
yangjiyoung <0112jyoung@naver.com>
2023-06-06 23:49:38 +0900

    feat: 로그인 경로 설정

--- src/main/java/com/with/picme/controller/AuthController.java
+++ src/main/java/com/with/picme/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.with.picme.controller;
 
 import com.with.picme.common.ApiResponse;
+import com.with.picme.dto.auth.AuthSignInRequestDto;
+import com.with.picme.dto.auth.AuthSignInResponseDto;
 import com.with.picme.dto.auth.AuthSignUpRequestDto;
 import com.with.picme.dto.auth.AuthSignUpResponseDto;
 import com.with.picme.service.AuthServiceImpl;
@@ -23,5 +25,11 @@ public class AuthController {
     public ResponseEntity<ApiResponse> createUser(@RequestBody AuthSignUpRequestDto request) {
         AuthSignUpResponseDto response = authService.createUser(request);
         return ResponseEntity.ok(ApiResponse.success(SUCCESS_SIGN_UP.getMessage(), response));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<ApiResponse> signInUser(@RequestBody AuthSignInRequestDto request) {
+        AuthSignInResponseDto response = authService.signInUser(request);
+        return ResponseEntity.ok(ApiResponse.success("회원 가입 성공", response));
     }
 }
